279a8b23d4a35b13bd932b47dc138b7c9a48dfa2
Your Namect git config --global user.email <roma.chikhladze777@gmail.com>
2020-04-14 16:15:37 +0400

    add sounds

--- simon/src/app/components/game/game.component.ts
+++ simon/src/app/components/game/game.component.ts
@@ -19,12 +19,19 @@ export class GameComponent implements OnInit {
     yellow: false
   }
 
+  sounds = {
+    green: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
+    red: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+    blue: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+    yellow: new Audio ("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"),
+  };
+
   constructor(private readonly gameStateService: GameStateService) { }
 
   ngOnInit() {
     this.gameStateService.state.subscribe(state => {
       console.log(state);
-      if(state.defeated) {
+      if(state.defeated && this.gameIsRunning) {
         this.count = 1;
         alert('you defeated try again');
         this.gameIsRunning = false;
@@ -40,17 +47,19 @@ export class GameComponent implements OnInit {
 
   async onPlayerGuess(color: string) {
     if(!this.playerMove){
-      alert('this is not your move')
       return;
     }
+    this.sounds[color].play();
     this.gameStateService.playerGuess(color);
-    if(this.count === this.gameStateService.count) {
-      await sleep(500);
+    if(this.gameStateService.player.length-1 === this.gameStateService.count){
+      console.log('egaaaaaaaaaaaaaaaaaaaaaaaaa');
+      await sleep(1000);
     }
   }
 
   async teasePlayer(simon: string[]) {
     for (let i = 0; i < simon.length; i++) {
+      this.sounds[simon[i]].play();
       this.colors[simon[i]] = true;
       await sleep(1000);
       this.colors[simon[i]] = false;
